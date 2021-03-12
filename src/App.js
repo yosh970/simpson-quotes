490784d690c6fefaf8e08d9e2a051a91bf2d5aea
Josselyn Mas <josh.jmas@gmail.com>
2021-03-12 18:46:34 +0100

    lifecyle quest

--- src/App.js
+++ src/App.js
@@ -2,36 +2,36 @@ import axios from 'axios';
 import React, {Component} from 'react';
 import Navbar from './components/Navbar';
 import QuoteCard from './components/QuoteCard';
-
-const startingQuote = {
-  image:"https://pm1.narvii.com/7070/aeaeadea89d89327a8e5f4f83d10272dc337425fr1-1200-1200v2_128.jpg",
-  character:"Nelson Muntz",
-  quote:"Shoplifting is a victimless crime, like punching someone in the dark.",
-  characterDirection:"Left"
-}
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      quote: startingQuote
+      quote: null
     }
     this.getQuote = this.getQuote.bind(this)
   }
 
+  componentDidMount() {
+    this.getQuote();
+  }
+
   getQuote(){
     axios.get("https://simpsons-quotes-api.herokuapp.com/quotes").then(res => {
-      console.log(res)
       this.setState({
         quote: res.data[0]
       })
-    })
+    }).catch(err => console.log(err))
   }
 
   render () {
     return (
       <div className="App">
         <Navbar />
-        <QuoteCard quote={this.state.quote} />
+        {
+          this.state.quote
+            ? <QuoteCard quote={this.state.quote} />
+            : <p>No Data yet</p>
+        }
         <button type="button"onClick={this.getQuote}>New Quote</button>
       </div>
     );
